c9f5d8e51dfa810287244bb8245959da1bd91e80
Brett C <brcannon@bellsouth.net>
2023-11-09 00:01:04 -0600

    updates to linkedin account

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <a href="https://github.com/brett-c">
                 <img src={GitHub} alt="GitHub Account" className="footer_image"></img>
             </a>
-            <a href="www.linkedin.com/in/brett-cannon-29a53a280" >
+            <a href="https://www.linkedin.com/in/brett-cannon-29a53a280" >
                 <img src={LinkedIn} alt="LinkedIn Account" className="footer_image"></img>
             </a>
         </div>
